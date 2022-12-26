8eff02f7e0fad4f30223ff03a5c961c409d4de2a
Michael Amore <michael.amore@yahoo.com>
2022-12-25 16:23:18 -0800

    Improve blog spacing.

--- app/components/MDX.tsx
+++ app/components/MDX.tsx
@@ -6,7 +6,7 @@ import Tip from './Tip';
 const components = {
   ExternalLink: (props: any) => <ExternalLink {...props} />,
   Tip: (props: any) => <Tip {...props} />,
-  h3: (props: any) => <h3 className={'mt-20'} {...props} />,
+  h3: (props: any) => <h3 className={'mt-28'} {...props} />,
   h5: (props: any) => <h5 className={'mt-6'} {...props} />,
   li: (props: any) => <li className={'ml-12 list-disc'} {...props} />,
 };
