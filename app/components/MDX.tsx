ce8d3fc471db1be76f27994f0d0cb02dfa54275a
Michael Amore <michael.amore@yahoo.com>
2022-12-28 20:42:50 -0800

    Update Vercel article.

--- app/components/MDX.tsx
+++ app/components/MDX.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { MDXRemote } from 'next-mdx-remote';
+import Image from 'next/image';
 import ExternalLink from './ExternalLink';
 import Tip from './Tip';
 import CodeWithLove from './CodeWithLove';
 
 const components = {
-  ExternalLink: (props: any) => <ExternalLink {...props} />,
   CodeWithLove: (props: any) => <CodeWithLove {...props} />,
+  ExternalLink: (props: any) => <ExternalLink {...props} />,
+  Image: (props: any) => <Image {...props} />,
   Tip: (props: any) => <Tip {...props} />,
   h2: (props: any) => <h2 className={'mb-0'} {...props} />,
   h3: (props: any) => <h3 className={'mt-28'} {...props} />,
