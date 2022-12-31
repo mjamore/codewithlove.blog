8882de4ff3b494e4e8d44cb502334d97a3e27603
Michael Amore <michael.amore@yahoo.com>
2022-12-30 21:48:35 -0800

    Redeploying again.

--- components/CommonHeadTags.tsx
+++ components/CommonHeadTags.tsx
@@ -3,10 +3,7 @@ export default function CommonHeadTags() {
     <>
       <meta charSet='utf-8' />
       <meta name='viewport' content='width=device-width, initial-scale=1, viewport-fit=cover' />
-      {/* To run the web application in full-screen */}
       <meta name='apple-mobile-web-app-capable' content='yes' />
-      {/* Status Bar Style (see Supported Meta Tags below for available values)
-      Has no effect unless you have the previous meta tag */}
       <meta name='apple-mobile-web-app-status-bar-style' content='black' />
       <link rel='apple-touch-icon' sizes='180x180' href='/favicons/apple-touch-icon.png' />
       <link rel='icon' type='image/png' sizes='32x32' href='/favicons/favicon-32x32.png' />
@@ -17,7 +14,6 @@ export default function CommonHeadTags() {
       <meta name='msapplication-TileColor' content='#444444' />
       <meta name='msapplication-config' content='/favicons/browserconfig.xml' />
       <meta name='theme-color' content='#444444' />
-
       <meta name='robots' content='follow, index' />
       <meta property='og:site_name' content='The Code With Love Blog' />
       <meta property='og:type' content='website' />
