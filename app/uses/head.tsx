9be871dc79adafaf3b973d9e42239346d0899080
Michael Amore <michael.amore@yahoo.com>
2022-12-30 18:17:26 -0800

    Add head section to uses page.

---
+++ app/uses/head.tsx
@@ -0,0 +1,28 @@
+import { URLS } from '../utils/constants';
+import CommonHeadTags from '../components/CommonHeadTags';
+
+export default function Head() {
+  const meta = {
+    title: "Michael Amore's Uses Page | The Code With Love Web Development Blog",
+    description: `A simple list of all of the different tools and technologies that I use on a daily basis.`,
+    image: '/images/logos/logo-amore-red-white.png',
+    editedDate: '12/30/2022',
+  };
+
+  return (
+    <>
+      <CommonHeadTags />
+      <title>{meta.title}</title>
+      <meta name='description' content={meta.description} />
+      <link rel='canonical' href={`${URLS.DOMAIN}/${URLS.ROUTES.ABOUT}`} />
+      <meta property='og:url' content={`${URLS.DOMAIN}/${URLS.ROUTES.ABOUT}`} />
+      <meta property='og:description' content={meta.description} />
+      <meta property='og:title' content={meta.title} />
+      <meta property='og:image' content={meta.image} />
+      <meta name='twitter:title' content={meta.title} />
+      <meta name='twitter:description' content={meta.description} />
+      <meta name='twitter:image' content={meta.image} />
+      {meta.editedDate && <meta property='article:published_time' content={meta.editedDate} />}
+    </>
+  );
+}
